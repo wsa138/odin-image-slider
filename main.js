89cee4c4d3e96382fd3092629939631d9e3d9b1a
William Addesso <wsaddesso@gmail.com>
2021-04-24 16:42:27 -0400

    Added event listener for left and right button clicks.

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 const images = Array.from(document.querySelectorAll('.images'));
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
 
 images[3].classList.add('framed');
+
+/* TODO: Function that runs when a direction arrow is 
+clicked that iterates though the images array the necessary 
+amount of times. */
+
+left.addEventListener('click', () => {
+  console.log('clicked left');
+});
+
+right.addEventListener('click', () => {
+  console.log('clicked right');
+});
