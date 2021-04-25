80f0fbbd5f41b3d97f9297dea7f2af5d0d7409d4
William Addesso <wsaddesso@gmail.com>
2021-04-25 15:31:37 -0400

    Changed argument names of functions.

--- main.js
+++ main.js
@@ -5,28 +5,30 @@ const right = document.querySelector('.right');
 images[0].classList.add('framed');
 
 // Find any images with class name 'framed' and remove it.
-const removeClassName = (rmvClass, arr) => {
+const removeClassName = (framedClass, arr) => {
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i].classList.contains(rmvClass)) {
-      arr[i].classList.remove(rmvClass);
+    if (arr[i].classList.contains(framedClass)) {
+      arr[i].classList.remove(framedClass);
       return arr.indexOf(arr[i]);
     }
   }
 };
 
 // Find currently framed image and shift to the left by one.
-const shiftLeft = (arr) => {
-  let oldIndex = removeClassName('framed', images);
+const shiftLeft = (framedClass, arr) => {
+  const oldIndex = removeClassName(framedClass, images);
   let newIndex;
   if (oldIndex === 0) {
     newIndex = arr.length - 1;
-    arr[newIndex].classList.add('framed');
+    arr[newIndex].classList.add(framedClass);
   } else {
     newIndex = oldIndex - 1;
-    arr[newIndex].classList.add('framed');
+    arr[newIndex].classList.add(framedClass);
   }
 };
 
 left.addEventListener('click', () => {
-  shiftLeft(images);
+  shiftLeft('framed', images);
 });
+
+right.addEventListener('click', () => {});
