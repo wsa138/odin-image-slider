4dfa4c6a41cc0bc62487527a5d150d032394c940
William Addesso <wsaddesso@gmail.com>
2021-04-25 17:13:44 -0400

    Added code to change the highlighted dot to correspond to the index of the image being displayed.

--- main.js
+++ main.js
@@ -1,34 +1,55 @@
 const images = Array.from(document.querySelectorAll('.images'));
+const dots = Array.from(document.querySelectorAll('.dot'));
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 
 images[0].classList.add('framed');
+dots[0].classList.add('highlight');
 
 // Find any images with class name 'framed' and remove it.
-const removeClassName = (framedClass, arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].classList.contains(framedClass)) {
-      arr[i].classList.remove(framedClass);
-      return arr.indexOf(arr[i]);
+const removeFramed = () => {
+  for (let i = 0; i < images.length; i++) {
+    if (images[i].classList.contains('framed')) {
+      images[i].classList.remove('framed');
+      dots[i].classList.remove('highlight');
+      return images.indexOf(images[i]);
     }
   }
 };
 
 // Find currently framed image and shift to the left by one.
-const shiftLeft = (framedClass, arr) => {
-  const oldIndex = removeClassName(framedClass, images);
+const shiftLeft = () => {
+  const oldIndex = removeFramed('framed', images);
   let newIndex;
   if (oldIndex === 0) {
-    newIndex = arr.length - 1;
-    arr[newIndex].classList.add(framedClass);
+    newIndex = images.length - 1;
+    images[newIndex].classList.add('framed');
+    dots[newIndex].classList.add('highlight');
   } else {
     newIndex = oldIndex - 1;
-    arr[newIndex].classList.add(framedClass);
+    images[newIndex].classList.add('framed');
+    dots[newIndex].classList.add('highlight');
   }
 };
 
-left.addEventListener('click', () => {
-  shiftLeft('framed', images);
+const shiftRight = () => {
+  const oldIndex = removeFramed('framed', images);
+  let newIndex;
+  if (oldIndex === 3) {
+    newIndex = 0;
+    images[newIndex].classList.add('framed');
+    dots[newIndex].classList.add('highlight');
+  } else {
+    newIndex = oldIndex + 1;
+    images[newIndex].classList.add('framed');
+    dots[newIndex].classList.add('highlight');
+  }
+};
+
+right.addEventListener('click', () => {
+  shiftRight();
 });
 
-right.addEventListener('click', () => {});
+left.addEventListener('click', () => {
+  shiftLeft();
+});
